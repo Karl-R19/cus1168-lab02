4e5ef6ea17b27d4ee30813f473aeb802b8ee996a
“Karl <karl.reid21@my.stjohns.edu>
2025-02-10 22:54:46 -0500

    finished homework

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -1,13 +1,99 @@
 package academy.javapro;
 
 public class Tesla extends Vehicle implements Electric, Autonomous {
-    private boolean autopilotEnabled;
-    private boolean charging;
+	private boolean autopilotEnabled;
+	private boolean charging;
 
-    // TODO: Create constructor that takes model and year
-    // TODO: Set make to "Tesla"
-    // TODO: Initialize autopilotEnabled and charging to false
+	public Tesla(String model, int year) {
+		super("Tesla", model, year);
+		this.autopilotEnabled = false;
+		this.charging = false;
+	}
 
-    // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
-    // Each method should include appropriate print statements
+	// staartEngine method from Vehicle class
+	public void startEngine() {
+		if (!isRunning) {
+			isRunning = true;
+			System.out.println("Tesla started.");
+
+		} else {
+			System.out.println("Tesla is already running.");
+		}
+	}
+
+	// stopEngine method from Vehicle class
+	@Override
+	public void stopEngine() {
+		if (isRunning) {
+			isRunning = false;
+			System.out.println("Tesla stopped.");
+		} else {
+			System.out.println("Tesla is already off.");
+		}
+	}
+
+	// accelerate method from Vehicle class
+	@Override
+	public void accelerate() {
+		if (isRunning) {
+			System.out.println("Tesla is accelerating.");
+		} else {
+			System.out.println("Start the tesla first before accelerating.");
+		}
+	}
+
+	// brake method from Vehicle class
+	@Override
+	public void brake() {
+		if (isRunning) {
+			System.out.println("Tesla is braking.");
+		} else {
+			System.out.println("Start the engine first before braking.");
+		}
+	}
+
+	// charge method from Electric class
+	@Override
+	public void charge() {
+		if (!charging) {
+			charging = true;
+			System.out.println("Tesla is charging.");
+		} else {
+			System.out.println("Tesla is already charging.");
+		}
+	}
+
+	// isCharging method from Electric class
+	@Override
+	public boolean isCharging() {
+		return charging;
+	}
+
+	// enableAutopilot method from Autonomous class
+	@Override
+	public void enableAutopilot() {
+		if (!autopilotEnabled) {
+			autopilotEnabled = true;
+			System.out.println("Autopilot enabled.");
+		} else {
+			System.out.println("Autopilot is already enabled.");
+		}
+	}
+
+	// disableAutopilot method from Autonomous class
+	@Override
+	public void disableAutopilot() {
+		if (autopilotEnabled) {
+			autopilotEnabled = false;
+			System.out.println("Autopilot disabled.");
+		} else {
+			System.out.println("Autopilot is already disabled.");
+		}
+	}
+
+	// isAutopilotEnabled method from Autonomous class
+	@Override
+	public boolean isAutopilotEnabled() {
+		return autopilotEnabled;
+	}
 }
--- src/main/java/academy/javapro/Vehicle.java
+++ src/main/java/academy/javapro/Vehicle.java
@@ -1,14 +1,67 @@
 package academy.javapro;
 
 public abstract class Vehicle {
-    protected String make;
-    protected String model;
-    protected int year;
-    protected boolean isRunning;
+	protected String make;
+	protected String model;
+	protected int year;
+	protected boolean isRunning;
 
-    // TODO: Add constructor
+	public Vehicle(String make, String model, int year) {
 
-    // TODO: Add abstract methods
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.isRunning = false;
+	}
 
-    // TODO: Add getters/setters
+	// Abstract methods to implement:
+	public abstract void startEngine();
+
+	public abstract void stopEngine();
+
+	public abstract void accelerate();
+
+	public abstract void brake();
+	
+
+		// Gathers the make of the vehicle
+		public String getMake() {
+		return make;
+		}
+
+		// Sets the make of the vehicle
+		public void setMake(String make) {
+		this.make = make;
+		}
+
+		// Gathers the model of the vehicle
+		public String getModel() {
+		return model;
+		}
+
+		// Sets the model of the vehicle
+		public void setModel(String model) {
+		this.model = model;
+		}
+
+		// Gathers the year of the vehicle
+		public int getYear() {
+		return year;
+		}
+
+		// Sets the year of the vehicle
+		public void setYear(int year) {
+		this.year = year;
+		}
+
+		// Check if the vehicle is running or not
+		public boolean isRunning() {
+		return isRunning;
+		}
+		
+		// Sets the running state of the vehicle
+		public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+		}
+
 }
